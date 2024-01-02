3e5ff4b4f68b7d7ab0b04eb7f81476e325a92fc9
Yishak <yishakdemsew@gmail.com>
2024-01-02 18:38:36 +0300

    added remote mysql database

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,13 +1,17 @@
+// require("dotenv").config();
 let mysql2 = require("mysql2");
 
-const dbConnection = mysql2.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "evangadidb",
+// const dbConnection = mysql2.createPool({
+//     host:   "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "evangadidb",
 
-    connectionLimit: 10, // maximum number of connections in the pool
-});
+//     connectionLimit: 10, // maximum number of connections in the pool
+// });
+
+urlDB = `mysql://root:Fh6fCdb2C4dGFe-65edFdbceeFA5FA1b@monorail.proxy.rlwy.net:43942/railway`;
+const dbConnection = mysql2.createPool(urlDB);
 
 // dbConnection.execute("select 'test' ", (err, result) => {
 //     if (err) {
