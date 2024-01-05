2d3a3da0d081fd46554a0ee756cc5a9e2071d4f4
Yishak <yishakdemsew@gmail.com>
2024-01-05 09:46:03 +0300

    config CORS middleware

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 5500;
 //json middleware to extract json data
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: "evangadi-forum-izak.netlify.app" }));
 
 let authMiddleware = require("./middleware/authMiddleware");
 //user routes middleware file
